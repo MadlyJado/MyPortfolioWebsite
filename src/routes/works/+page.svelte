a24f0c22cd38d8716d1eb74084516db06e9d8fa7
MadlyJado <jadonchenard@gmail.com>
2023-04-06 13:17:26 -0400

    Working on editing stuffs

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -7,8 +7,7 @@
 <p class="text-center">My greatest achievement is a game jam I did back in the summer of 2021.</p>
 <ul class="text-center">
     <li>We made it in 2 weeks!</li>
-    <li>I was in charge of the level design and coming up with a save/load system.</li>
-    <li>I could not create a save/load system in time but together we got a different system</li>
+    <li>I was in charge of the level design and some of the coding</li>
     <li>We used our other programmer's global variable system to keep track of the user's info through levels</li>
     <li>It basically became a rogue-like!</li>
     <li>In two weeks, we created most of the enemies, the GUI, and even a final boss!</li>
