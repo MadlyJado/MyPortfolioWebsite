5353c15bde329de5f1d4574ae48d82be15793fd5
MadlyJado <jadonchenard@gmail.com>
2023-04-06 20:17:49 -0400

    Added screenshots to show images of game jam

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     import PortfolioPageComponent from "../../components/portfoliopage.svelte";
+
+    import moonshaunted1 from "$lib/assets/moonshaunted1.png";
+
+    import moonshaunted2 from "$lib/assets/moonshaunted2.png";
+
+    import moonshaunted3 from "$lib/assets/moonshaunted3.png";
+
+    import moonshaunted4 from "$lib/assets/moonshaunted4.png";
 </script>
 
 <PortfolioPageComponent worksActive="active"/>
@@ -9,7 +17,11 @@
     <li>We made it in 2 weeks!</li>
     <li>I was in charge of the level design and some of the coding</li>
     <li>We used our other programmer's global variable system to keep track of the user's info through levels</li>
-    <li>It basically became a rogue-like!</li>
+    <li><img src={moonshaunted1} alt=""></li>
+    <li><img src={moonshaunted2} alt=""></li>
+    <li><img src={moonshaunted3} alt=""></li>
+    <li><img src={moonshaunted4} alt=""></li>
+    <li>It basically became a rogue-like rpg</li>
     <li>In two weeks, we created most of the enemies, the GUI, and even a final boss!</li>
     <li><a href="https://ferventprayers.itch.io/moons-haunted">Click Here to play the game jam game!</a></li>
 </ul>
