b6613ef503c1f727ba7d97171a40a05099490a09
MadlyJado <jadonchenard@gmail.com>
2023-04-17 17:27:41 -0400

    Updated gh-pages js file for gh pages publishing

--- gh-pages.js
+++ gh-pages.js
@@ -4,6 +4,13 @@ ghpages.publish(
     'public',
     {
         branch: 'gh-pages',
-        repo: 
+        repo: 'https://github.com/MadlyJado/MyPortfolioWebsite.git',
+        user: {
+            name: 'MadlyJado',
+            email: 'jadonchenard@gmail.com'
+        }
+    },
+    () => {
+        console.log("Deploy Complete!")
     }
 )
