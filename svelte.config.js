2426d591ba2c6d1b1e62355b604968bd8283964b
MadlyJado <jadonchenard@gmail.com>
2023-08-31 21:14:25 -0400

    added .nojekyll file

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-node';
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-	},
-};
+    preprocess: vitePreprocess(),
 
-export default config;
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
+    }
+};
